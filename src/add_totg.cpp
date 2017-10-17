30a17276a78f2a6469a2a39d4e6cbfb562a5b459
Ruben Burger <r.b.burger@tudelft.nl>
2017-10-17 21:06:42 +0200

    Cleanup and removed README.md

--- src/add_totg.cpp
+++ src/add_totg.cpp
@@ -1,47 +1,36 @@
-#include <moveit/planning_request_adapter/planning_request_adapter.h>
 #include <class_loader/class_loader.h>
-#include <ros/ros.h>
+#include <moveit/planning_request_adapter/planning_request_adapter.h>
 #include <ros/console.h>
-// #include <moveit_topp/moveit_topp.h>
-// #include <rosbag/bag.h>
+#include <ros/ros.h>
 
-// #include <moveit_topp/spline_fitting.h>
-
-#include <trajectories/Trajectory.h>
 #include <trajectories/Path.h>
+#include <trajectories/Trajectory.h>
 
 using namespace Eigen;
 using namespace std;
 
-namespace totg_planning_adapter
-{
+namespace totg_planning_adapter {
 
-class AddTOTG : public planning_request_adapter::PlanningRequestAdapter
-{
+class AddTOTG : public planning_request_adapter::PlanningRequestAdapter {
 public:
-  AddTOTG() : planning_request_adapter::PlanningRequestAdapter(), nh_("~")
-  {
-    ROS_INFO_STREAM("test");
-  }
+  AddTOTG() : planning_request_adapter::PlanningRequestAdapter(), nh_("~") {}
 
   ~AddTOTG(){};
 
-  virtual std::string getDescription() const { return "Add TOTG"; }
+  virtual std::string getDescription() const {
+    return "TOTG Planning Request Adapter";
+  }
 
   virtual bool
   adaptAndPlan(const PlannerFn &planner,
                const planning_scene::PlanningSceneConstPtr &planning_scene,
                const planning_interface::MotionPlanRequest &req,
                planning_interface::MotionPlanResponse &res,
-               std::vector<std::size_t> &added_path_index) const
-  {
+               std::vector<std::size_t> &added_path_index) const {
     bool result = planner(planning_scene, req, res);
-
     bool totg_result = false;
-    ROS_INFO_STREAM("Starting TOTG param");
 
-    if (result && res.trajectory_)
-    {
+    if (result && res.trajectory_) {
 
       list<VectorXd> waypoints;
       double acceleration_scaling_factor = 1.0;
@@ -59,14 +48,12 @@ public:
       VectorXd maxAcc(vars_count);
       VectorXd maxVel(vars_count);
 
-      for (size_t i = 0; i < rt->getWayPointCount(); i++)
-      {
+      for (size_t i = 0; i < rt->getWayPointCount(); i++) {
         rt->getWayPoint(i).copyJointGroupPositions(jmg, waypoint);
         waypoints.push_back(waypoint);
       }
 
-      for (int i = 0; i < vars_count; i++)
-      {
+      for (int i = 0; i < vars_count; i++) {
         // Get Acceleration limit
         double a_max = 1.0;
         const robot_model::VariableBounds &a =
@@ -87,17 +74,16 @@ public:
         maxVel[i] = v_max;
       }
 
-      Trajectory trajectory(Path(waypoints, 0.1), maxVel, maxAcc);
+      double timestep = 0.001; // Decreasing this should provide better paths,
+                               // but the parameterization takes longer
+
+      Trajectory trajectory(Path(waypoints, 0.1), maxVel, maxAcc, timestep);
 
       totg_result = trajectory.isValid();
 
       robot_state::RobotState rs(rt->getRobotModel());
 
-      if (totg_result)
-      {
-
-        ROS_INFO_STREAM("valid result");
-
+      if (totg_result) {
         double step_size = 0.025;
         double duration = trajectory.getDuration();
         rt->clear();
@@ -110,19 +96,7 @@ public:
         rs.setVariableVelocities(vars, velocities);
         rt->addSuffixWayPoint(rs, 0.0);
 
-        bool toggle = true;
-        for (double t = 0.0; t < duration; t += step_size)
-        {
-          if (toggle)
-          {
-            ROS_INFO_STREAM("OMG IM IN A LOOP: " << t);
-            toggle = !toggle;
-          }
-          else
-          {
-            ROS_WARN_STREAM("OMG IM IN A LOOP: " << t);
-            toggle = !toggle;
-          }
+        for (double t = 0.0; t < duration; t += step_size) {
           Eigen::VectorXd vel_vec = trajectory.getVelocity(t);
           std::vector<double> velocities(
               vel_vec.data(), vel_vec.data() + vel_vec.rows() * vel_vec.cols());
@@ -131,14 +105,10 @@ public:
           rs.setVariableVelocities(vars, velocities);
           rt->addSuffixWayPoint(rs, step_size);
         }
-      }
-      else
-      {
-        ROS_WARN_STREAM("Trajectory generation failed!");
+      } else {
+        ROS_WARN_STREAM("TOTG failed!");
       }
     }
-
-    ROS_INFO_STREAM("Done with TOTG Param");
     return result && totg_result;
   }
 
